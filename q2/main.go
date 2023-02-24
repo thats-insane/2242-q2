c2915877a23f24ec46285ea8917330c01ffe306a
krismic <2021154337@ub.edu.bz>
2023-02-24 10:42:43 -0600

    mixed up some code, finally done

--- q2/main.go
+++ q2/main.go
@@ -5,13 +5,18 @@ import (
 	"time"
 )
 
-func displayMessage(input string) {
-	fmt.Println("Important announcement:", input)
+func importMessage(input chan string) {
+	input <- "Belize is going to blow up in 45 minutes."
+}
+
+func displayMessage(input chan string) {
+	fmt.Println("Important announcement:", <-input)
 }
 
 func main() {
-	message := "Belize is going to blow up in 45 minutes."
+	message := make(chan string)
 
+	go importMessage(message)
 	go displayMessage(message)
 
 	time.Sleep(1 * time.Second)
